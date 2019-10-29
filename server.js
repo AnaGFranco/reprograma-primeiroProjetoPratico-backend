6d7547b9b0217048c136ea217b2a2e5020a6482c
AnaGFranco <anagsfranco@hotmail.com>
2019-10-28 21:40:47 -0300

    Criando a estrutura do projeto

---
+++ server.js
@@ -0,0 +1,11 @@
+const app = require("./src/app")
+
+const port = 3000
+
+app.listen(port, function() {
+    console.log(`app está rodando na porta ${port}`)   //boas praticas usar crase por causa de alguns caracter
+    
+    })
+
+
+server.listen(3000);
