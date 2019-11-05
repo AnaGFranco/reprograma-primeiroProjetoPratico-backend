30b25a787114be9916f62b66c1894bd50f7e8876
AnaGFranco <anagsfranco@hotmail.com>
2019-11-04 21:34:36 -0300

    Convertendo string em data para calcular a duração da tarefa

--- src/controllers/tarefasController.js
+++ src/controllers/tarefasController.js
@@ -53,69 +53,23 @@ exports.getByDataInclusao =(req, res) =>{
     res.status(200).send(teste)
 }
 
-exports.getDuracao =(req, res) =>{
-
-    const duracao = tarefas.map((item) => {
-        let dtSeparadaInclusao = item.dataInclusao.split('/')
-        let novaDtInclusao = (dtSeparadaInclusao[2] + "-" + dtSeparadaInclusao[1] + "-" + dtSeparadaInclusao[0]);
-        let dataConvertidaInclusao = new Date(novaDtInclusao)
-
-        let dtSeparadaConclusao = item.dataConclusao.split('/')
-        let novaDtConclusao = (dtSeparadaConclusao[2] + "-" + dtSeparadaConclusao[1] + "-" + dtSeparadaConclusao[0]);
-        let dataConvertidaConclusao = new Date(novaDtConclusao)
-
-         return item.duracao = calcularDuracao(dataConvertidaInclusao, dataConvertidaConclusao);
-        
-    })
-
-    res.status(200).send(duracao)
-}
-
-
-function calcularDuracao(dataInclusao, dataConclusao) {
-    let dateConclu = new Date(dataConclusao)
-    let dateInclu= new Date(dataInclusao)
-
-    let duracao = dateConclu - dateInclu;
-    console.log(dateConclu + ' ' + dateInclu +' '+ duracao)
-
-    return duracao;
-}
-
-// exports.getAge = (req, res) => {
-//     const id = req.params.id
-//     const aluna = alunas.find(item => item.id == id)
-//     const dataNasc = aluna.dateOfBirth
-//     const arrData = dataNasc.split("/")
-//     const dia = arrData[0]
-//     const mes = arrData[1]
-//     const ano = arrData[2]
-//     const idade = calcularIdade(ano, mes, dia)
-//     res.status(200).send({ idade })
-//   }
-  
-//   function calcularIdade(anoDeNasc, mesDeNasc, diaDeNasc) {
-//     const now = new Date()
-//     const anoAtual = now.getFullYear()
-//     const mesAtual = now.getMonth() + 1
-//     const hoje = now.getDate()
-  
-//     let idade = anoAtual - anoDeNasc
-  
-//     if (mesAtual < mesDeNasc || (mesAtual == mesDeNasc && hoje < diaDeNasc)) {
-//       idade -= 1
-//     }
-//     return idade
-//   }
-
-// function copy(o){
-//     var copy = Object.create( Object.getPr(ototypeOf(o) );
-//     var propNames = Object.getOwnPropertyNames(o);
-//     propNames.forEach(function(name){
-//       var desc = Object.getOwnPropertyDescriptor(o, name);
-//       Object.defineProperty(copy, name, desc);
-  
-//     });
-  
-//     return copy;
-//   }
+exports.getDuracao = (req, res) => {
+    tarefas.forEach(item => {
+      // element.outraChave = "a";
+      const diferencaTempo = Math.abs(
+        new Date(
+            item.dataConclusao.split("/")[2],
+            item.dataConclusao.split("/")[1],
+            item.dataConclusao.split("/")[0]
+        ) -
+          new Date(
+            item.dataInclusao.split("/")[2],
+            item.dataInclusao.split("/")[1],
+            item.dataInclusao.split("/")[0]
+          )
+      );
+      const diferencaDias = Math.ceil(diferencaTempo / (1000 * 60 * 60 * 24));
+      item.tempoLevado = diferencaDias;
+    });
+    res.status(200).send(tarefas);
+};
